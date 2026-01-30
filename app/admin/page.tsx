039ad7efb06c9ecec256caccb8fe29596f6e6399
giovanaassis <giovana_2011_11@hotmail.com>
2026-01-30 00:36:31 -0300

    fix: disable cache on admin dashboard to reflect updated data

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,6 +5,8 @@ import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 
+export const revalidate = 0;
+
 export default async function AdminPage() {
   const appointments = await getRecentAppointmentList();
 
